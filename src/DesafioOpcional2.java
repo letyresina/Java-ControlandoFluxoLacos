a7a0aaaae2a90ce46ae5c14a92078b10a1c54360
letyresina <leticiaresina@gmail.com>
2023-03-20 21:12:27 -0300

    Subindo Desafio Opcional 2

---
+++ src/DesafioOpcional2.java
@@ -0,0 +1,13 @@
+
+public class DesafioOpcional2 {
+	public static void main(String[] args) {
+		int fatorial = 1;
+		int n;
+		
+		for (n = 1; n <= 10; n++) {
+			fatorial *= n;
+			System.out.println("O fatorial de " + n + " = " + fatorial);
+		}
+			
+	}
+}
