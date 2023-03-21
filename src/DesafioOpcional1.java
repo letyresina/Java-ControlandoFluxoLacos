f55d97ec5603e24accbe3ffff607e050719089cd
letyresina <leticiaresina@gmail.com>
2023-03-20 21:07:32 -0300

    Subindo o desafio opcional 1

---
+++ src/DesafioOpcional1.java
@@ -0,0 +1,11 @@
+
+public class DesafioOpcional1 {
+	public static void main(String[] args) {
+		int i;
+		for(i = 0; i <= 100; i++) {
+			if(i % 3 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
